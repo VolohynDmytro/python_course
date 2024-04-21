3e04d7d16a3dfe57d20b7d311bb35a38de6eceb0
VolohynDmytro <voloshindima777@gmail.com>
2024-04-21 23:38:02 +0300

    решение дз 2.1 в ls2hm.py и 2.2 в ls2hm2.py

--- 02_lesson/ls2hm.py
+++ 02_lesson/ls2hm.py
@@ -1,3 +1,20 @@
-num_input = input('Введите 4-х значное число')
 
-print(num_input)
+
+
+
+num_input = input('!!!!!Введите целое 4-х значное число:  ')
+
+a = int(num_input)
+b = a//1000
+print(b)
+
+c = a//100
+d = c - b*10
+print(d)
+
+e = a//10
+f = e - c*10
+print(f)
+
+g = a//1 - e*10
+print(g)
