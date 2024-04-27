77b49505520d1c9fded603c932041b7f464b2dda
VolohynDmytro <voloshindima777@gmail.com>
2024-04-27 20:58:17 +0300

    исправил домашку 3.1 в ls3hw1

--- 03_lesson/ls3hw1.py
+++ 03_lesson/ls3hw1.py
@@ -10,12 +10,12 @@ if operation == '*':
     result = num1_input * num2_input
     print(result)
 
-elif operation == '/' and num2_input != 0:
-    result = num1_input / num2_input
-    print(result)
-
 elif operation == '/' and num2_input == 0:
     print(' На ноль делится')
+
+elif operation == '/':
+    result = num1_input / num2_input
+    print(result)
 
 elif operation == '+':
     result = num1_input + num2_input
