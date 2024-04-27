75c8a11079c68a11d48dc6a654dd1b8742bc3455
VolohynDmytro <voloshindima777@gmail.com>
2024-04-27 20:55:47 +0300

    исправил домашку 3.1 в ls3hw1

--- 03_lesson/ls3hw1.py
+++ 03_lesson/ls3hw1.py
@@ -1,25 +1,26 @@
-result = None
+
 
 num1_input = int(input("Введите число для делениея: "))
 
-num2_input = int(input("Введите делитель(кроме нуля): "))
+num2_input = int(input("Введите второе число: "))
 
-if num2_input != 0:
+operation = input("Выберите операцию: \b Деление '/' \b Умножение '*' \b Сочитание '+' \b Вычитание '-' ")
 
-    operation = input("Выберите операцию: \b Деление '/' \b Умножение '*' \b Сочитание '+' \b Вычитание '-' ")
+if operation == '*':
+    result = num1_input * num2_input
+    print(result)
 
-    if operation == '*':
-        result = num1_input * num2_input
-        print(result)
-    elif operation == '/':
-        result = num1_input / num2_input
-        print(result)
-    elif operation == '+':
-        result = num1_input + num2_input
-        print(result)
-    elif operation == '-':
-        result = num1_input - num2_input
-        print(result)
+elif operation == '/' and num2_input != 0:
+    result = num1_input / num2_input
+    print(result)
 
-else:
-    print('ДЕЛИТЕЛЬ НЕ ДОЛЖЕН РАВНЯТЬСЯ НУЛЮ')
+elif operation == '/' and num2_input == 0:
+    print(' На ноль делится')
+
+elif operation == '+':
+    result = num1_input + num2_input
+    print(result)
+
+elif operation == '-':
+    result = num1_input - num2_input
+    print(result)
